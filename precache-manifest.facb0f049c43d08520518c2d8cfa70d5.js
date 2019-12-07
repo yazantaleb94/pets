d72330e729194f02fcd9afc3da4414ea4feb436e
umuttalipoglu94 <yazan.taleb@ogr.sakarya.edu.tr>
2019-12-07 15:35:53 +0300

    Updates

---
+++ precache-manifest.facb0f049c43d08520518c2d8cfa70d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4f42cfeddd1e3d9622215b52c5545b9",
+    "url": "/pets/index.html"
+  },
+  {
+    "revision": "5c2ba63656ae0ce9e9dc",
+    "url": "/pets/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "c0b1970b3457e9b7c3d8",
+    "url": "/pets/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "5c2ba63656ae0ce9e9dc",
+    "url": "/pets/static/js/2.1df0c754.chunk.js"
+  },
+  {
+    "revision": "c0b1970b3457e9b7c3d8",
+    "url": "/pets/static/js/main.bac39515.chunk.js"
+  },
+  {
+    "revision": "f16eb5c8fca67b5e6023",
+    "url": "/pets/static/js/runtime-main.142f7f8b.js"
+  }
+]);
